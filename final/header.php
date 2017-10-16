999d272ea8d79606f55e2bf81ec3d0c9ad87387d
Ivan Rosario <ivanrosario@Administrators-MacBook-Pro-2.local>
2017-10-16 12:36:09 -0400

    viewport taken out

--- final/header.php
+++ final/header.php
@@ -13,7 +13,6 @@ $_SESSION['cart'];
 	<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>  
 	<link href="lib/css/stylesheet.css" rel="stylesheet" type="text/css">	
 	<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 
