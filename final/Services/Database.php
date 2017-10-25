9a28db9da416ecec2740ead31f45ad7f0e0425da
Ivan Rosario <ivanrosario@Administrators-MacBook-Pro-2.local>
2017-10-24 22:11:34 -0400

    db change

--- final/Services/Database.php
+++ final/Services/Database.php
@@ -1,16 +1,16 @@
 <?php
-	try{
-	$db = new PDO("mysql:host=localhost;dbname=Finalphp;", "root", "root");
-	} catch(Exception $e){
-			echo "unable to connect oh ";
-			exit;
-	}
-
 	// try{
-	// 	$db = new PDO("mysql:host=localhost;dbname=iRosario_Final;", "r2hstudent", "SbFaGzNgGIE8kfP");
+	// $db = new PDO("mysql:host=localhost;dbname=Finalphp;", "root", "root");
 	// } catch(Exception $e){
-	// 	echo "unable to connect oh ";
-	// 	exit;
+	// 		echo "unable to connect oh ";
+	// 		exit;
 	// }
+
+	try{
+		$db = new PDO("mysql:host=localhost;dbname=iRosario_Final;", "r2hstudent", "SbFaGzNgGIE8kfP");
+	} catch(Exception $e){
+		echo "unable to connect oh ";
+		exit;
+	}
 
 ?>
